b7a83ea4a51cc24e5fbb08f9d78327fe0716e7b6
Abel Chacko Mathew <achusep19@gmail.com>
2024-08-21 00:03:34 -0500

    Form looks good; minor changes required

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, request
 from pymongo import MongoClient
 import config
 
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017, username=config.dbUser, password=config.dbPassword)
-
-
 db = client.flask_db
 todos = db.todos
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+@app.route('/', methods=('GET', 'POST'))
+def main():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
