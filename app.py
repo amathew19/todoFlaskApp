91d699a4436257f27f88e05150acec4893fae0e4
Abel Chacko Mathew <achusep19@gmail.com>
2024-08-22 14:50:53 -0500

    Created a web page for adding and displaying todos; Fixed issue with pymongo client

--- app.py
+++ app.py
@@ -4,14 +4,21 @@ import config
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017, username=config.dbUser, password=config.dbPassword)
-db = client.flask_db
+client = MongoClient(username=config.dbUser, password=config.dbPassword, authSource="todoFlaskDB")
+db = client['todoFlaskDB']
 todos = db.todos
 
 
 @app.route('/', methods=('GET', 'POST'))
 def main():
-    return render_template('index.html')
+    if request.method == 'POST':
+        content = request.form['content']
+        degree = request.form['degree']
+        todos.insert_one({'content': content, 'degree': degree})
+        return redirect(url_for('main'))
+
+    all_todos=todos.find()
+    return render_template('index.html', todos=all_todos)
 
 
 if __name__ == '__main__':
